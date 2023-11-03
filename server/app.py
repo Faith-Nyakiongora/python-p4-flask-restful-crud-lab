26aa6e1cd081a1db12a7898c3b42506a377746c3
+ <+>
2023-11-03 09:40:18 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -3,11 +3,14 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from models import db  
+from models import Plant
 
 from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -30,6 +33,7 @@ class Plants(Resource):
             name=data['name'],
             image=data['image'],
             price=data['price'],
+            is_in_stock=True
         )
 
         db.session.add(new_plant)
@@ -41,11 +45,35 @@ class Plants(Resource):
 api.add_resource(Plants, '/plants')
 
 
+
+
 class PlantByID(Resource):
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            data = request.get_json()
+            plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+            db.session.commit()
+            return make_response(plant.to_dict(), 200)
+        else:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('', 204)
+        else:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+api.add_resource(Plants, '/plants')
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
